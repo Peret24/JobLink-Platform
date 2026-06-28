131746d94442f512c26e019cd1f3e011ec9c0f5d
Alena <peretejkoalena@gmail.com>
2026-06-28 23:56:20 +0300

    Initial commit: JobLink platform 2

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,11 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect('mongodb+srv://kuyfuy0_db_user:PApvIiRhimK6XjS5@cluster0.7gnls1a.mongodb.net/?appName=Cluster0')
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/joblink_demo';
+
+mongoose.connect(MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB error:', err));
-
 // --- СХЕМЫ ---
 
 const userSchema = new mongoose.Schema({
